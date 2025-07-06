73195526fd5f0d5e1865cc43245c4250c3a981e0
raghavsingh-01 <raghavchauhan772@gmail.com>
2025-07-06 07:45:15 +0530

    Add hello_world.py - first Python program

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#hello_world.py
+#Prints a greeting and user's name
+# Author: raghavsingh-01
+
+print('Hello World!')
+print('What is your name?')
+name=input()
+
+print('It is good to meet you,'+ name)
+print('Hope you are good and well.')

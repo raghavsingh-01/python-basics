8d34605b64dd4129bd05dfe55166cc81db962f00
raghavsingh-01 <raghavchauhan772@gmail.com>
2025-07-24 22:43:26 +0530

    Add files via upload

---
+++ swap_numbers.py
@@ -0,0 +1,15 @@
+#This is the code for swapping of two numbers
+num1 = input("Enter the value of first number :")
+num2 = input("Enter the value of second number :")
+
+print("The value of number 1 is " + num1)
+print("The value of number 2 is " + num2)
+
+#Swapping
+temp = num1
+num1 = num2
+num2 = temp
+
+print("The values after swapping :-")
+print("The value of number 1 is " + num1)
+print("The value of number 2 is " + num2)

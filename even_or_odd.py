5e691919a6e5d04fcc3d1cb8e8471c098d634b07
raghavsingh-01 <raghavchauhan772@gmail.com>
2025-07-24 22:16:14 +0530

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+# This is the simple program to determine the number is odd or even
+print("Enter the number: ")
+num = int(input())
+if num == 0 :
+    print("Whole number")
+elif num%2==0 :
+    print("Even number")
+else :
+    print("Odd number")

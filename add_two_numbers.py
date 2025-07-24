19c9a83a6aa5e3d70c933e8de7baab5fee633e46
raghavsingh-01 <raghavchauhan772@gmail.com>
2025-07-24 22:12:07 +0530

    Add files via upload

---
+++ add_two_numbers.py
@@ -0,0 +1,9 @@
+#This the simple program to find the sum of two entered numbers.
+print("Enter two numbers to find sum!")
+print("1st:")
+one = int(input())
+print("2nd:")
+two = int(input())
+sum = one + two
+sum = str(sum)
+print('The Sum of two entered value is ' + sum)
